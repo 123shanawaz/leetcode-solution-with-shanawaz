944186b108b9a9674486ba1bbaae66c24aff35c8
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2021-12-21 01:12:38 +0530

    Time: 28 ms (72.03%), Space: 25 MB (47.31%) - LeetHub

--- rotate-array/rotate-array.cpp
+++ rotate-array/rotate-array.cpp
@@ -12,6 +12,16 @@ public:
     }
     void rotate(vector<int>& nums, int k) {
         k=k%nums.size();
+      /*  
+        2 3 4 5 1// k=-1
+        1 2 3 4 5 //
+        5 1 2 3 4 //1 r
+        4 5 1 2 3 //2 r
+        3 4 5 1 2 //3 r
+        2 3 4 5 1//4 r
+        1 2 3 4  5 // r
+        */    
+            
         if(k<0){
             k=k+nums.size();
         }
