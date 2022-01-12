411a8485f6c4206f5a3cfb422a08a609ea8f755e
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-01-12 13:16:39 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (10.10%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-   int findfibonacci(int n){
-        if(n==0)
-            return 0;
-        if(n==1) 
-            return 1;
+  
+    int find_fibonacci(int n,unordered_map<int,int> &mp){
+       
+        if(n==0)  return 0;
+        if(n==1)  return 1;
+        int currentkey = n;
+        if(mp.find(currentkey)!=mp.end()){
+          return mp[currentkey];
+        }
+        int ans1 = find_fibonacci(n-1,mp);
+        int ans2 = find_fibonacci(n-2,mp);
+        mp[currentkey] = ans1+ans2;
+        return mp[currentkey];
         
-        int ans1=findfibonacci(n-1);
-        int ans2=findfibonacci(n-2);
-    return ans1+ans2;
-   }
+    }
     int fib(int n) {
-        return findfibonacci(n);
+        unordered_map<int,int> mp;
+        return find_fibonacci(n,mp);
     }
 };
