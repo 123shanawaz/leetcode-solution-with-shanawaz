320f3c958134cbba5b7934f40fe05e67c13ebec6
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-01-15 22:42:46 +0530

    Time: 3 ms (49.14%), Space: 8.9 MB (5.79%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxrob(vector<int>& nums,int n,int currentidx,unordered_map<int,int>&mp){
+        if(currentidx>=n){
+            return 0;
+        }
+        int currentkey=currentidx;
+        if(mp.find(currentidx)!=mp.end()){
+            return mp[currentidx];
+        }
+        int lotenge=nums[currentidx]+maxrob(nums,n,currentidx+2,mp);
+        int nhilotenge=maxrob(nums,n,currentidx+1,mp);
+        mp[currentidx]= max(lotenge,nhilotenge);
+        return mp[currentidx];
+}
+    
+    int rob(vector<int>& nums) {
+        unordered_map<int,int>mp,mp1;
+        int n=nums.size();
+        if(n==1){
+            return nums[0];
+        }
+        int first = maxrob(nums,n-1,0,mp);
+        int second = maxrob(nums,n,1,mp1);
+
+       return max(first,second);
+        
+    }
+};
