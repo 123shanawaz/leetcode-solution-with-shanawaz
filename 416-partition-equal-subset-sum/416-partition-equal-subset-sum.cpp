6c18b0311d2914d144fc526c3a3246cd5642dc8e
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-01-17 02:00:34 +0530

    Time: 216 ms (33.50%), Space: 39.3 MB (21.84%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool isPossibleToForm(vector<int>&nums,int currentIndex, int target,unordered_map<string,int>&mp){
+        
+        if(target==0){
+            return true;
+        }
+        
+        if(currentIndex >= nums.size()){
+            return false;
+        }
+          string currentKey = to_string(currentIndex) + "_" + to_string(target);
+        if(mp.find(currentKey)!=mp.end()){
+           return mp[currentKey];
+        }
+
+
+        
+        bool pick = false;
+        
+
+        if(nums[currentIndex] <= target){
+            pick = isPossibleToForm(nums, currentIndex+1,target-nums[currentIndex],mp);
+// if humlog ka target >= nums[curIdx] 
+        }
+        if(pick){
+        mp[currentKey]=true;
+            return mp[currentKey];
+        }
+        bool notPick = isPossibleToForm(nums,currentIndex+1,target,mp);
+        
+         mp[currentKey] =pick || notPick;
+        return mp[currentKey];
+        
+    }
+    bool canPartition(vector<int>& nums) {
+        unordered_map<string,int>mp;
+        int sum = 0;
+        for(auto i: nums){
+            sum+=i;
+        }
+        if(sum%2!=0){
+            return false;
+        }
+        int target = sum/2;
+        return isPossibleToForm(nums,0,target,mp);
+    }
+};
+
