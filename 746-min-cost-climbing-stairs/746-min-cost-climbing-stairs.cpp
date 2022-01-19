aba20c2f9c9bb99ce8136cf9b14f0c1f0acbdda3
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-01-19 19:49:27 +0530

    Time: 12 ms (25.31%), Space: 15.6 MB (5.38%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    int mincost(int current,int n,vector<int>& cost,unordered_map<int,int> &mp){
-      
-      // when we reach the last index/top, return 0, 0 is the cost of the top
-      if(current==n)  return 0;
-      // when we reach > the last index/top, return INT_MAX/cost.length
-      if(current>n)   return INT_MAX;
-      
-      // take the changing variable as the key
-      int currentkey = current;
-      // check if the key is the present in the map or not
-      if(mp.find(currentkey)!=mp.end()){
-        return mp[currentkey];
-      }
-      
-      // either we can take 1 step, current+1
-      int one_step = mincost(current+1,n,cost,mp);
-      // or we can take 2 step, current+2
-      int two_step = mincost(current+2,n,cost,mp);
-      
-      // take the minimum of jump + cost of the current as mp[key]
-      mp[currentkey] = cost[current]+min(one_step,two_step);
-      return mp[currentkey];
+    
+   int mincost(vector<int>& cost,int currentidx,unordered_map<int,int>&mp){
+       if(cost.size()==currentidx){
+           return 0;
+   }
+    if(currentidx>cost.size()){
+        return INT_MAX;
     }
-    int minCostClimbingStairs(vector<int>& cost){
-        int n = cost.size();
-        unordered_map<int,int> mp;
-      
-        // we can take jump from either 0 or 1
-        // so calculate the minimum of 0 and 1
-        return min(mincost(0,n,cost,mp),mincost(1,n,cost,mp));
+       int currentkey=currentidx;
+       if(mp.find(currentkey)!=mp.end()){
+           return mp[currentkey];
+       }
+    int onejump=mincost(cost,currentidx+1,mp);
+     int twojump=mincost(cost,currentidx+2,mp);
+       mp[currentkey]=min(onejump,twojump)+cost[currentidx];
+       return mp[currentkey];
+   }
+    int minCostClimbingStairs(vector<int>& cost) {
+        unordered_map<int,int>mp;
+        int a=mincost(cost,0,mp);
+        int b=mincost(cost,1,mp);
+        return min(a,b);
     }
 };
