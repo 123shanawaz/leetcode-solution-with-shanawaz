f8fe8ebc58083f08887576c6c0dee6f3cec72928
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-01-22 01:54:53 +0530

    Time: 240 ms (32.31%), Space: 90.4 MB (14.54%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int dp[100001][2][3];
+    int bestTime(vector<int>&prices, int currentDay, bool canBuy, int transCount){
+        if(currentDay >= prices.size() || transCount <=0){
+            return 0;
+        }
+        
+        if(dp[currentDay][canBuy][transCount]!=-1){
+            return dp[currentDay][canBuy][transCount];
+        }
+        int idle = bestTime(prices,currentDay+1,canBuy,transCount);
+        int buy=0,sell=0;
+        if(canBuy){
+             buy = -prices[currentDay] + bestTime(prices,currentDay+1, 0,transCount);
+            dp[currentDay][canBuy][transCount] =  max(buy,idle);
+            return  dp[currentDay][canBuy][transCount];
+        }
+        else{
+            sell = prices[currentDay] + bestTime(prices,currentDay+1,1,transCount-1);
+            dp[currentDay][canBuy][transCount] =  max(sell,idle);
+            return  dp[currentDay][canBuy][transCount];
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        memset(dp,-1,sizeof(dp));
+        return bestTime(prices,0,1,2);
+    }
+};
