509df127aaf0eb0d43e717c3972beb800714edc9
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-01-29 00:58:48 +0530

    Time: 4 ms (40.54%), Space: 6.8 MB (32.77%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mp[10001];
+    int totalWays(vector<int>&nums, int currentIndex,int target){
+        if(currentIndex >= nums.size()|| target<=0){
+            return (target==0) ? 1:0;
+        }
+      
+        if(mp[target]!=-1){
+            return mp[target];
+        }
+        int pick =  totalWays(nums,0, target - nums[currentIndex]);
+        int notPick = totalWays(nums, currentIndex+1, target);
+        mp[target] = pick+notPick;
+        return mp[target];
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        memset(mp,-1,sizeof(mp));
+        return totalWays(nums,0,target);
+    }
+};
