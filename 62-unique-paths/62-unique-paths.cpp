908c7515c260fdcb639069a948aab03e36486c6b
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-01-28 22:28:17 +0530

    Time: 24 ms (5.31%), Space: 9 MB (23.66%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int path(int m,int n,int i,int j,unordered_map<string,int>&mp){
+    int maxpath(int m,int n,int i,int j,unordered_map<string,int>&mp){
         if(i>=m||j>=n){
             return 0;
         }
         if(i==m-1&&j==n-1){
             return 1;
         }
-        string currentkey=to_string(i)+"_"+to_string(j);
-        if(mp.find(currentkey)!=mp.end()){
-            return mp[currentkey];
+        string idx=to_string(i)+"_"+to_string(j);
+        if(mp.find(idx)!=mp.end()){
+            return mp[idx];
         }
-        int right=path(m,n,i,j+1,mp);
-        int left=path(m,n,i+1,j,mp);
-        mp[currentkey]= right+left;
-        return mp[currentkey];
-        
+        int row=maxpath(m,n,i+1,j,mp);
+        int down=maxpath(m,n,i,j+1,mp);
+        mp[idx]= row+down;
+        return mp[idx];
     }
     int uniquePaths(int m, int n) {
         unordered_map<string,int>mp;
-        return path(m,n,0,0,mp);
+        return maxpath(m,n,0,0,mp);
         
     }
 };
