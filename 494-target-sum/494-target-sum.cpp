c330ab6e458d5ca958c13a508c80dbe0afa74111
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-01-28 23:15:32 +0530

    Time: 961 ms (22.37%), Space: 89.8 MB (5.66%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    int totalway(vector<int>& nums, int target,int index){
+    int totalway(vector<int>& nums, int target,int index,unordered_map<string,int>&mp){
         if(target==0 && index==nums.size() ){
             return 1;
         }
         if(index>=nums.size() && target!=0){
             return 0;
         }
-        int pos=totalway(nums,target-nums[index],index+1);
-        int neg=totalway(nums,target+nums[index],index+1);
-        return pos+neg;
+        string currentkey=to_string(index)+"_"+to_string(target);
+        if(mp.find(currentkey)!=mp.end()){
+            return mp[currentkey];
+        }
+        int pos=totalway(nums,target-nums[index],index+1,mp);
+        int neg=totalway(nums,target+nums[index],index+1,mp);
+        mp[currentkey]= pos+neg;
+           return mp[currentkey];
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return totalway(nums,target,0);
+        unordered_map<string,int>mp;
+        return totalway(nums,target,0,mp);
         
     }
 };
