488f9a3788f9c7eef93ce09527311e38db16dc3d
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-02-28 14:06:51 +0530

    Time: 3 ms (84.46%), Space: 12.8 MB (63.13%) - LeetHub

--- 872-leaf-similar-trees/872-leaf-similar-trees.cpp
+++ 872-leaf-similar-trees/872-leaf-similar-trees.cpp
@@ -11,21 +11,35 @@
  */
 class Solution {
 public:
-    void getleavs(TreeNode* root,vector<int>&v){
-        if(root==NULL){
-            return ;
-        }
-        if(root->left==NULL && root->right==NULL){
+     void preorderLeafSequence(TreeNode* root,vector<int> & v)
+    {
+        if(root==NULL)
+            return;
+        // if the node is NULL store it in the vector
+		if(root->left==NULL && root->right==NULL)
             v.push_back(root->val);
-        }
-        getleavs(root->left,v);
-        getleavs(root->right,v);
+        // preorder traversal steps
+		preorderLeafSequence(root->left,v);
+        preorderLeafSequence(root->right,v);
     }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int>a,b;
-        getleavs(root1,a);
-        getleavs(root2,b);
-        return a==b;
-        
+       // two vectors to store leaf sequence
+	   vector<int>  v1,v2;
+        preorderLeafSequence(root1,v1);
+        preorderLeafSequence(root2,v2);
+		// if vector are of different size return false
+        if(v1.size()!=v2.size())
+            return false;
+		
+        for(int i=0;i<v1.size();i++)
+        {
+            if(v1[i]!=v2[i])
+                return false;
+        }
+		// if all elements of the vector are same return true
+        return true;
     }
+        
+    
+
 };
