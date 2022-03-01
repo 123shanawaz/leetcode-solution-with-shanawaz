feaa1328df8d053fb7a7c2f22642fba51cceef91
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-03-02 02:18:13 +0530

    Time: 11 ms (32.09%), Space: 7.7 MB (89.00%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res(n + 1);
+        res[0] = 0;
+        for(int i = 1;i <= n;i++){
+            res[i] = res[(i >> 1)] + (i & 1);
+        }
+        return res;
+    }
+};
