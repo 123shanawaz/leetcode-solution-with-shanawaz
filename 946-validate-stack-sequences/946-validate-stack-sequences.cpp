c9d828cc07a4aba3abb5620655b4c7057fbe551a
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-03-16 13:35:27 +0530

    Time: 8 ms (83.81%), Space: 15.4 MB (59.16%) - LeetHub

---
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int>st;
+        int j=0;
+        for(auto p:pushed){
+            st.push(p);
+            while(!st.empty() && j<popped.size() && st.top()==popped[j]){
+                st.pop();
+                j++;
+            }
+        }
+        return j==popped.size();
+    }
+};
