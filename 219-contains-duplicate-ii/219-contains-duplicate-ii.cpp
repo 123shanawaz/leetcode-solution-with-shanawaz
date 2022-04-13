8cc1c7a0715937dd371bbc891c204a5d9e4e7b55
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-04-14 00:37:46 +0530

    Time: 172 ms (87.25%), Space: 77.1 MB (51.58%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -3,7 +3,7 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int, int> mp;
         for(int i = 0; i < nums.size(); i++) {
-            if(mp.count(nums[i])){
+            if(mp.find(nums[i])!=mp.end()){
                 int diff = abs(mp[nums[i]] - i);
                 if (diff <= k)return true;  
             }
