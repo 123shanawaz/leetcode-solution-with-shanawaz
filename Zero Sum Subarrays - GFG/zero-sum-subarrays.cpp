c721f3254d644ab0d1048949a14f553bc0c91fc4
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-04-17 15:04:29 +0530

    Added solution - LeetHub

--- Zero Sum Subarrays - GFG/zero-sum-subarrays.cpp
+++ Zero Sum Subarrays - GFG/zero-sum-subarrays.cpp
@@ -15,11 +15,10 @@ class Solution{
         long long ans=0;
          long long sum=0;
          unordered_map<int,int>mp;
+         mp[0]=1;
         for(int i=0;i<n;i++){
             sum+=arr[i];
-            if(sum==0){
-                ans++;
-            }
+            
                if(mp.find(sum)!=mp.end()){
                    ans+=mp[sum]++;
                }
