7f90f1aa9aa1f14b34662d073f669f84facc565a
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-05-23 17:22:58 +0530

    Time: 0 ms (100.00%), Space: 7 MB (31.74%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -3,40 +3,45 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        int left=0,right=m-1,bottom=n-1,top=0;
+        int left=0;
+        int right=m-1;
+        int top=0;
+        int bottom=n-1;
         int direction=1;
-        vector<int> v;
-        while(left<=right && top<=bottom)
-        {
-            if(direction==1)
-            {
-                for(int i=left;i<=right;i++) v.push_back(matrix[top][i]);
-                direction=2;
+        vector<int>ans;
+        while(left<=right && top<=bottom){
+            if(direction==1){
+            for(int index=left;index<=right;index++)
+                ans.push_back(matrix[top][index]);
                 top++;
-            }
+                direction=2;
             
-            else if(direction==2)
-            {
-                for(int i=top;i<=bottom;i++) v.push_back(matrix[i][right]);
-                direction=3;
+            }
+            else if(direction==2){
+                for(int index=top;index<=bottom;index++)
+                ans.push_back(matrix[index][right]);
                 right--;
-            }
+                direction=3;
             
-            else if(direction==3)
-            {
-                for(int i=right;i>=left;i--) v.push_back(matrix[bottom][i]);
-                direction=4;
+            }
+                else if(direction==3){
+                    for(int index=right;index>=left;index--)
+                ans.push_back(matrix[bottom][index]);
                 bottom--;
-            }
+                direction=4;
             
-            else if(direction==4)
-            {
-                for(int i=bottom;i>=top;i--) v.push_back(matrix[i][left]);
-                direction=1;
+                }
+            else if(direction==4){
+                for(int index=bottom;index>=top;index--)
+                ans.push_back(matrix[index][left]);
                 left++;
+                direction=1;
+            
             }
+                
+            
         }
-        return v;
+    return ans;
         
     }
 };
