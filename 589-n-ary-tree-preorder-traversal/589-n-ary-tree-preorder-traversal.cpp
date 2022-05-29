26bc7f3945b08665c03bb5207b9ca8922ddd17db
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-05-29 22:12:51 +0530

    Time: 44 ms (18.83%), Space: 11.2 MB (48.97%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
@@ -21,16 +21,19 @@ public:
 class Solution {
 public:
     void minpreorder(Node* root,vector<int>&ans){
-        if(root==NULL)return ;
+        if(root==NULL){
+            return ;
+        }
         ans.push_back(root->val);
         for(int i=0;i<root->children.size();i++){
-        minpreorder(root->children[i],ans);
-            }
+            minpreorder(root->children[i],ans);
+        }
         return;
     }
     vector<int> preorder(Node* root) {
         vector<int>ans;
         minpreorder(root,ans);
-        return ans;        
+        return ans;
+        
     }
 };
