742b54c3f11cc39f90d7398858cb2cbe02fdd458
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-06-01 12:42:31 +0530

    Time: 5 ms (50.37%), Space: 12.5 MB (97.11%) - LeetHub

--- 872-leaf-similar-trees/872-leaf-similar-trees.cpp
+++ 872-leaf-similar-trees/872-leaf-similar-trees.cpp
@@ -11,35 +11,24 @@
  */
 class Solution {
 public:
-     void preorderLeafSequence(TreeNode* root,vector<int> & v)
-    {
-        if(root==NULL)
-            return;
-        // if the node is NULL store it in the vector
-		if(root->left==NULL && root->right==NULL)
-            v.push_back(root->val);
-        // preorder traversal steps
-		preorderLeafSequence(root->left,v);
-        preorderLeafSequence(root->right,v);
-    }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-       // two vectors to store leaf sequence
-	   vector<int>  v1,v2;
-        preorderLeafSequence(root1,v1);
-        preorderLeafSequence(root2,v2);
-		// if vector are of different size return false
-        if(v1.size()!=v2.size())
-            return false;
-		
-        for(int i=0;i<v1.size();i++)
-        {
-            if(v1[i]!=v2[i])
-                return false;
-        }
-		// if all elements of the vector are same return true
-        return true;
+        vector<int>a;
+        vector<int>b;
+        isSame(root1,a);
+        isSame(root2,b);
+        return a==b;
     }
-        
-    
-
+    private:void isSame(TreeNode* root, vector<int>&v){
+        if(root==NULL)return ;
+        if(root->left==NULL && root->right==NULL){
+            v.push_back(root->val);
+        }
+        if(root->left!=NULL){
+            isSame(root->left,v);
+        }
+        if(root->right!=NULL){
+            isSame(root->right,v);
+        }
+        return;
+    }
 };
