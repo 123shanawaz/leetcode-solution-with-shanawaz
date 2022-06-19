03b65acdc626cbfa092f4b3317505b4977654395
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-06-19 17:11:25 +0530

    Time: 56 ms (87.11%), Space: 23.9 MB (91.95%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) 
+    {
+        sort(products.begin(),products.end());
+        vector<vector<string>> result;
+        int start,bsStart=0;
+        string curr;
+        for(char &c:searchWord)
+        {
+            curr+=c;        
+            start=lower_bound(products.begin()+bsStart,products.end(),curr)-products.begin();//Get the starting index of word starting with prefix `curr`.
+            result.push_back({});
+            for(int i=start;i<min(start+3,(int)products.size())&&!products[i].compare(0,curr.size(),curr);i++)//Add the words with the same prefix to the result.
+                result.back().push_back(products[i]);
+            bsStart=start;				//Reduce the size of elements to binary search on.
+        }
+        return result;
+    }
+};
