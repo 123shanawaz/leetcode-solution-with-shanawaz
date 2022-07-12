21540597b3e18dad464bef883c8c63f47c01e9d3
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-07-12 18:29:25 +0530

    Time: 9 ms (20.98%), Space: 8.3 MB (25.71%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
-   bool uniqueOccurrences(vector<int>& arr) {
-  unordered_map<int, int> m;
-  unordered_set<int> s;
-  for (auto n : arr) ++m[n];
-  for (auto& p : m) s.insert(p.second);
-  return m.size() == s.size();
-}
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int>mp;
+        set<int>s;
+        for(int i=0;i<arr.size();i++){
+            mp[arr[i]]++;
+        }
+        for(auto a:mp){
+            s.insert(a.second);
+        }
+        return mp.size()==s.size();
+    }
 };
