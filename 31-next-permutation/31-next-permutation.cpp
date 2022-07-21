4e26ad1627d10a501f66388ab01f50cdc0af6248
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-07-21 17:42:56 +0530

    Time: 7 ms (57.78%), Space: 12.1 MB (36.60%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,9 +1,32 @@
 class Solution{
 public:
     void nextPermutation(vector<int>& nums) {
-	    // Optimal Solution Time O(N) & Auxiliary Space O(1)
-	    // Inbuilt Function
-        next_permutation(nums.begin(),nums.end());
+	    if(nums.size()==0){
+            return ;
+        }
+        int idx1;
+        int n=nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+            idx1=i;
+            break;
+            }
+        }
+        if(idx1<0){
+            sort(nums.begin(),nums.end());
+        }
+        else{
+            int idx2;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>nums[idx1]){
+                idx2=i;
+                break;
+            }
+                
+        }
+            swap(nums[idx1],nums[idx2]);
+            reverse(nums.begin()+idx1+1,nums.end());
+        }
     }
 };
 
