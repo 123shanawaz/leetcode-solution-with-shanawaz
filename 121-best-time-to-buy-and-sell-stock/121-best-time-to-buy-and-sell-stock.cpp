1e59c96422d464238e7be4c3366e474156f3954a
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-07-25 13:49:10 +0530

    Time: 290 ms (5.85%), Space: 93.3 MB (52.30%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    int dp[100001][2][2];
-    int bestTime(vector<int>&prices, int currentDay, bool canBuy, int transCount){
-        if(currentDay >= prices.size() || transCount <=0){
-            return 0;
-        }
-        
-        if(dp[currentDay][canBuy][transCount]!=-1){
-            return dp[currentDay][canBuy][transCount];
-        }
-        int idle = bestTime(prices,currentDay+1,canBuy,transCount);
-        int buy=0,sell=0;
-        if(canBuy){
-             buy = -prices[currentDay] + bestTime(prices,currentDay+1, 0,transCount);
-            dp[currentDay][canBuy][transCount] =  max(buy,idle);
-            return  dp[currentDay][canBuy][transCount];
-        }
-        else{
-            sell = prices[currentDay] + bestTime(prices,currentDay+1,1,transCount-1);
-            dp[currentDay][canBuy][transCount] =  max(sell,idle);
-            return  dp[currentDay][canBuy][transCount];
-        }
-    }
     int maxProfit(vector<int>& prices) {
-        memset(dp,-1,sizeof(dp));
-        return bestTime(prices,0,1,1);
+        int n=prices.size();
+        int maxR=prices[n-1];
+      int  max_profit=0;
+        for(int i=n-2;i>=0;i--){
+            maxR=max(maxR,prices[i]);
+                max_profit=max( max_profit,maxR-prices[i]);
+            
+        }
+        return max_profit;
+        
     }
 };
