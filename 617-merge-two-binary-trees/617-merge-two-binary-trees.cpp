5bb70861dbdeb7664a286c109e057ccb471f0070
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-08-19 00:07:25 +0530

    Time: 56 ms (62.89%), Space: 32.5 MB (43.72%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -9,25 +9,30 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution { // iterative: Stack
 public:
-    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        return mergebt(root1,root2);
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if(!t1 && !t2) return nullptr;
+        if(!t1 || !t2) return t1? t1:t2;
+				
+        stack<TreeNode*> s1,s2;
+        s1.push(t1);
+        s2.push(t2);
         
-    }
-    private:TreeNode* mergebt(TreeNode* root1, TreeNode* root2){
-        if(root1==NULL && root2==NULL){
-            return NULL ;
+        while(!s1.empty()){
+            TreeNode* c1(s1.top());
+            TreeNode* c2(s2.top());
+            s1.pop();
+            s2.pop();
+            
+            c1->val+=c2->val;
+            
+            if(!c1->left && c2->left) c1->left = c2->left;
+            else if(c1->left && c2->left) { s1.push(c1->left); s2.push(c2->left); }
+            
+            if(!c1->right && c2->right) c1->right = c2->right;
+            else if(c1->right && c2->right) { s1.push(c1->right); s2.push(c2->right); }
         }
-        if(root1==NULL){
-            return root2;
-        }
-        if(root2==NULL){
-            return root1;
-        }
-        TreeNode *newNode=new TreeNode(root1->val+root2->val);
-        newNode->left=mergebt(root1->left,root2->left);
-        newNode->right=mergebt(root1->right,root2->right);
-        return newNode;
+        return t1;
     }
 };
