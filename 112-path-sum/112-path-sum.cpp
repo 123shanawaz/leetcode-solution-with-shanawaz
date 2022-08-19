359bf0f8b82c725c39ba936fb5b9e8ca58a9a7de
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-08-19 18:58:49 +0530

    Time: 11 ms (87.15%), Space: 21.5 MB (39.51%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -12,23 +12,28 @@
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-        return pathsum(root,0,targetSum);
-        
-    }
-    private: bool pathsum(TreeNode* root,int currsum,int targetSum){
+        queue<TreeNode*>q;
         if(root==NULL){
             return false;
         }
-        if(root->left==NULL && root->right==NULL){
-            if((currsum+root->val)==targetSum){
-                return true ;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* p=q.front();
+            q.pop();
+
+            if(p->left==NULL && p->right==NULL){
+                if(p->val==targetSum)return true;
             }
-            else {
-                return false;
+            if(p->left!=NULL){
+                p->left->val+=p->val;
+                q.push(p->left);
+            }
+             if(p->right!=NULL){
+                p->right->val+=p->val;
+                q.push(p->right);
             }
         }
-        bool a= pathsum(root->left,currsum+root->val,targetSum);
-           bool b= pathsum(root->right,currsum+root->val,targetSum);
-        return a||b;
+        return false;
+        
     }
 };
