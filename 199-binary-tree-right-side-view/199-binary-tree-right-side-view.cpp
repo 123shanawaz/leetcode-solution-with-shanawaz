1f5525e1293ac6ba4dd8b8abb776571b224e4b85
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-08-22 12:12:55 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (44.67%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -11,22 +11,31 @@
  */
 class Solution {
 public:
-    void Rview(TreeNode *root,set<int>&s,vector<int>&ans,int currlavel){
-    if(root==NULL){
-        return ;
-    }
-    if(s.find(currlavel)==s.end()){
-        s.insert(currlavel);
-        ans.push_back(root->val);
-    }
-    Rview(root->right,s,ans,currlavel+1);
-    Rview(root->left,s,ans,currlavel+1);
-    return ;
-    }
+    vector<int> ans;
+    
     vector<int> rightSideView(TreeNode* root) {
-        vector<int>ans;
-        set<int>s;
-        Rview(root,s,ans,0);
+        if(root==NULL)return ans;
+        
+		queue<TreeNode*>q;
+        q.push(root);
+        
+		// Level order transversal
+		while(!q.empty()){
+            int s=q.size();
+            int dat=0;
+			
+            while(s){
+                TreeNode*temp=q.front();
+                q.pop();
+                dat=temp->val;
+                if(temp->left!=NULL)q.push(temp->left);
+                if(temp->right!=NULL)q.push(temp->right);
+                s--;
+            }
+			
+			// Simply push ush the last value
+            ans.push_back(dat);
+        }
         return ans;
     }
 };
