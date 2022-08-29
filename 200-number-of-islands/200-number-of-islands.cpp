dc7addb56f343e193bd372b03ae67dc460024bfa
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-08-30 00:40:38 +0530

    Time: 62 ms (41.45%), Space: 12.4 MB (55.87%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,36 +1,48 @@
 class Solution {
 public:
-    
-     void sinkiland(vector<vector<char>>& grid,int i,int j,int m, int n){
-        if(i<0||i>=m||j<0||j>=n||grid[i][j]=='0'){
-            return;
-        }
-        grid[i][j]='0';
-        sinkiland(grid,i-1,j,m,n);
-        sinkiland(grid,i+1,j,m,n);
-        sinkiland(grid,i,j-1,m,n);
-        sinkiland(grid,i,j+1,m,n);
-        return;
-        
-    }
     int numIslands(vector<vector<char>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        int ans=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]=='1'){
-                    ans++;
-                    sinkiland(grid,i,j,m,n);
+        int n=grid.size();
+        if(n==0)
+            return 0;
+        int m=grid[0].size();
+        
+        stack<pair<int,int>>s;
+        int count=0;
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                if(grid[i][j]=='1'){ // not visited, once visited or not land grid[i][j]="0"
+                    ++count;// also increment count of islands
+                    s.push(make_pair(i,j));
+
+                    while(!s.empty()){
+                        
+                        int x=s.top().first;
+                        int y=s.top().second;
+                        s.pop();
+                        
+                        grid[x][y]='0';// visit
+
+                        //move right
+                        if(y+1<m && grid[x][y+1]=='1')
+                            s.push(make_pair(x,y+1));
+                        
+                        // move down
+                        if(x+1<n && grid[x+1][y]=='1')
+                            s.push(make_pair(x+1,y));
+                        
+                        // move up
+                        if(x-1>=0 && grid[x-1][y]=='1')
+                            s.push(make_pair(x-1,y));
+                        
+                        // move left
+                        if(y-1>=0 && grid[x][y-1]=='1')
+                            s.push(make_pair(x,y-1));
+    
+                    }
                 }
             }
         }
-            
         
-        return ans;
-        
+        return count;
     }
-    
-    
-
 };
