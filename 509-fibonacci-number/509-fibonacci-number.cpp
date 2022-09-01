22695640ee17e3de0097c260151d717cac46f091
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-09-01 11:32:44 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (8.60%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     int fib(int n) {
-        return fibonacci(n);
+        unordered_map<int,int>mp;
+        return fibonacci(n,mp);
     }
-    private: int fibonacci(int n){
+    private: int fibonacci(int n,unordered_map<int,int>&mp){
         if(n==0){
             return 0;
         }
         if(n==1){
             return 1;
         }
-        int one=fibonacci(n-1);
-        int two=fibonacci(n-2);
-        return one+two;
+        int currkey=n;
+        if(mp.find(currkey)!=mp.end()){
+            return mp[currkey];
+        }
+        int one=fibonacci(n-1,mp);
+        int two=fibonacci(n-2,mp);
+        mp[currkey]= one+two;
+        return mp[currkey];
     }
 };
