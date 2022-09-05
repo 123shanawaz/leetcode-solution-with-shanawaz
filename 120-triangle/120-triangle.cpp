d7f20723359858addbf68ccebb34ee1260bfd79b
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-09-05 17:00:04 +0530

    Time: 98 ms (6.20%), Space: 13.7 MB (5.17%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-   int solve(int i, int j, int n, vector<vector<int>>&triangle, vector<vector<int>>&dp) {
+   int solve(int i, int j, int n, vector<vector<int>>&triangle, unordered_map<string,int>&dp) {
 		if(i == n - 1)
 			return triangle[i][j];
-		if(dp[i][j] != -1)
-			return dp[i][j];
+		
+       string currkey=to_string(i)+"_"+to_string(j);
+       if(dp.find(currkey)!=dp.end()){
+           return dp[currkey];
+       }
 		int bottom = triangle[i][j] + solve(i+1, j, n, triangle, dp);
 		int  bottom_right = triangle[i][j] + solve(i+1, j+1, n, triangle, dp);
-		return dp[i][j] = min(bottom, bottom_right);
+		 dp[currkey] = min(bottom, bottom_right);
+       return dp[currkey];
 	}
 	int minimumTotal(vector<vector<int>>& triangle) {
 		int n = triangle.size();
-		vector<vector<int>>dp(n, vector<int>(n, -1));
+		unordered_map<string,int>dp;
 		return solve(0, 0, n, triangle, dp);
 	}
 };
