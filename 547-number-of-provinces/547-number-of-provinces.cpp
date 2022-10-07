af054617d5f64a1453e648b4873493c6a38b3597
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-10-08 01:19:43 +0530

    Time: 31 ms (78.97%), Space: 15.5 MB (7.96%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,47 +1,51 @@
 class Solution {
 public:
- void dfs(int node,vector<int>& visited,vector<int> adjList[])
-    {
-       
-        for(auto it: adjList[node])
-        {
-            if(!visited[it])
-            {
-                visited[it]=1;
-                dfs(it,visited,adjList);
-            }
-        }
-    }
-    int findCircleNum(vector<vector<int>>& isConnected) 
-    {
-        int v=isConnected.size();
-        vector<int> visited(v,0);
-        vector<int> adjList[v];
-		
-        // converting adjency matrix to list **
-		
-        for(int i=0;i<v;i++)
-        {
-            for(int j=0;j<v;j++)
-            {
-                if(isConnected[i][j]==1 and i!=j)
-                {
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);
+int findCircleNum(vector<vector<int>>& isConnected) {
+        
+        int n = isConnected.size();
+        
+        vector<int>adj[n];
+        vector<int>vis(n,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j && isConnected[i][j]!=0){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
-        int count=0;
-        for(int i=0;i<v;i++)
-        {
-            if(!visited[i])
-            {
-                count++;
-                dfs(i,visited,adjList);
+        vector<int>ans;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            
+            if(vis[i])continue;
+
+           cnt++;
+        
+            vis[0]=1;
+
+            queue<int>store;
+
+            store.push(i); 
+
+            vector<int>ans;
+
+            while(!store.empty()){
+
+                int node = store.front();
+
+                store.pop();
+
+                ans.push_back(node);
+
+                for(auto it: adj[node]){
+                    if(!vis[it]){
+                        vis[it]=1;
+                        store.push(it);
+                    }
+                }
             }
         }
-        
-        return count;
+        return cnt;        
     }
-   
 };
