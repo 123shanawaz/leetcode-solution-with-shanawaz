4bfa97f1357f3eb524eb59095a2f4e48d7ba5292
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-10-10 13:19:50 +0530

    Time: 68 ms (42.58%), Space: 12.2 MB (82.79%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,48 +1,30 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
+    int numIslands(vector<vector<char>>& grid) { 
         int n=grid.size();
-        if(n==0)
-            return 0;
         int m=grid[0].size();
-        
-        stack<pair<int,int>>s;
         int count=0;
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                if(grid[i][j]=='1'){ // not visited, once visited or not land grid[i][j]="0"
-                    ++count;// also increment count of islands
-                    s.push(make_pair(i,j));
-
-                    while(!s.empty()){
-                        
-                        int x=s.top().first;
-                        int y=s.top().second;
-                        s.pop();
-                        
-                        grid[x][y]='0';// visit
-
-                        //move right
-                        if(y+1<m && grid[x][y+1]=='1')
-                            s.push(make_pair(x,y+1));
-                        
-                        // move down
-                        if(x+1<n && grid[x+1][y]=='1')
-                            s.push(make_pair(x+1,y));
-                        
-                        // move up
-                        if(x-1>=0 && grid[x-1][y]=='1')
-                            s.push(make_pair(x-1,y));
-                        
-                        // move left
-                        if(y-1>=0 && grid[x][y-1]=='1')
-                            s.push(make_pair(x,y-1));
-    
-                    }
+        for(int i=0;i<n;i++){
+            
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    count++;
+                    solve(grid,n,m,i,j);
                 }
             }
         }
-        
         return count;
+        
     }
+    private:void solve(vector<vector<char>>& grid,int n,int m,int i,int j){
+        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]=='0'){
+    return ;
+    }
+        grid[i][j]='0';
+        solve(grid,n,m,i-1,j);
+        solve(grid,n,m,i+1,j);
+        solve(grid,n,m,i,j+1);
+        solve(grid,n,m,i,j-1);
+        return;
+}
 };
