bad60615ba681007678f687684024a3f8852f930
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-10-16 22:10:18 +0530

    Time: 160 ms (37.07%), Space: 21.7 MB (42.58%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,35 +2,39 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        if(grid[0][0] == 1){
+        int m=grid[0].size();
+        if(grid[0][0]==1){
             return -1;
         }
-        vector<vector<int>> visited(n, vector<int>(n, -1));
-        visited[0][0] = 1;
-        
-        int arri[8] = {-1, -1, 0, +1, +1, +1, 0, -1};
+        if(grid[0][0]==0 && m==1 && n==1) return 1;
+      
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+           queue<pair<pair<int,int>,int>>q;
+      q.push({{0,0},1});
+         int arri[8] = {-1, -1, 0, +1, +1, +1, 0, -1};
         int arrj[8] = {0, +1, 1, 1, 0, -1, -1, -1};
-        
-        queue<pair<int, int>> q;
-        q.push({0,0});
-        
-        while(!q.empty()){
-            pair<int, int> temp = q.front(); q.pop();
-            int x = temp.first;
-            int y = temp.second;
+      while(!q.empty()){
+          int x=q.front().first.first;
+        int y = q.front().first.second;
+         int step=q.front().second;
+            q.pop();
             
             for(int k=0; k<8; k++){
                 int i = x + arri[k];
                 int j = y + arrj[k];
                 
-                if(i>=0 && j>=0 && i<n && j<n && grid[i][j] == 0 && visited[i][j] == -1){
-                    visited[i][j] = visited[x][y] + 1;
-                    q.push({i, j});
+                if(i>=0 && j>=0 && i<n && j<m && grid[i][j] == 0 && !vis[i][j]){
+                  
+                    q.push({{i, j},step+1});
+                      vis[i][j] = 1;
+                    if(i==n-1 && j==m-1){
+                        return step+1;
+                    }
                 }
             }
         }
-        
-        return visited[n-1][n-1];
+        return -1;
+      
         
     }
         
