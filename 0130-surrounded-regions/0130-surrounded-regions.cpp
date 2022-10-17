7ede9092bd7529203a026c644b62a7e652c68f76
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-10-17 17:00:55 +0530

    Time: 16 ms (81.20%), Space: 10.5 MB (21.86%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,45 +1,46 @@
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
-        int n=board.size();
+        int n =board.size();
         int m=board[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        for(int j=0;j<m;j++){
-            if(board[0][j]=='O' && !vis[0][j]){
-                dfs(0,j,vis,board,n,m);
-            }
-            if(board[n-1][j]=='O' && !vis[n-1][j]){
-                dfs(n-1,j,vis,board,n,m);
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(board[i][0]=='O' && !vis[i][0]){
-                dfs(i,0,vis,board,n,m);
-            }
-            if(board[i][m-1]=='O' && !vis[i][m-1]){
-                dfs(i,m-1,vis,board,n,m);
-            }
-        }
+         vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<int,int>>q;
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j]=='O' && !vis[i][j]){
+                if(i==0||j==0||i==n-1||j==m-1){
+                    if(board[i][j]=='O'){
+                        q.push({i,j});
+                        vis[i][j]=1;
+                    }
+                    else{
+                        vis[i][j]=0;
+                    }
+                }
+            }
+        }
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+             int drow[]={-1,0,+1,0};
+             int dcol[]={0,+1,0,-1};
+            for(int i=0;i<4;i++){
+                int nrow=row+drow[i];
+                int ncol=col+dcol[i];
+             if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && board[nrow][ncol]=='O'){
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol]=1;
+                }
+            }
+        }
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==0 && board[i][j]=='O'){
                     board[i][j]='X';
                 }
             }
         }
         
-        
-    }
-    private:void dfs(int row,int col, vector<vector<int>>&vis,vector<vector<char>>& board,int n,int m){
-        vis[row][col]=1;
-        int drow[]={-1,0,+1,0};
-        int dcol[]={0,+1,0,-1};
-        for(int i=0;i<4;i++){
-            int nrow=row+drow[i];
-            int ncol=col+dcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && board[nrow][ncol]=='O'){
-                dfs(nrow,ncol,vis,board,n,m);
-            }
-        }
     }
 };
