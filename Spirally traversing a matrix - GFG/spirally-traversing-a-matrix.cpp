91197085260c3ab4ade17fbad26534b46d184323
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2023-01-18 12:36:01 +0530

    Added solution - LeetHub

--- Spirally traversing a matrix - GFG/spirally-traversing-a-matrix.cpp
+++ Spirally traversing a matrix - GFG/spirally-traversing-a-matrix.cpp
@@ -1,55 +1,48 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h> 
 using namespace std; 
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution
 {   
     public: 
     //Function to return a list of integers denoting spiral traversal of matrix.
     vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
     {
-       int n=matrix.size();
-        int m=matrix[0].size();
-        int left=0,right=m-1,bottom=n-1,top=0;
-        int direction=1;
-        vector<int> v;
-        while(left<=right && top<=bottom)
-        {
-            if(direction==1)
-            {
-                for(int i=left;i<=right;i++) v.push_back(matrix[top][i]);
-                direction=2;
-                top++;
-            }
-            
-            else if(direction==2)
-            {
-                for(int i=top;i<=bottom;i++) v.push_back(matrix[i][right]);
-                direction=3;
-                right--;
-            }
-            
-            else if(direction==3)
-            {
-                for(int i=right;i>=left;i--) v.push_back(matrix[bottom][i]);
-                direction=4;
-                bottom--;
-            }
-            
-            else if(direction==4)
-            {
-                for(int i=bottom;i>=top;i--) v.push_back(matrix[i][left]);
-                direction=1;
-                left++;
-            }
-        }
-        return v;
-        
+       int left=0;
+       int right=c-1;
+       int top=0;
+       int bottom =r-1;
+       vector<int>ans;
+       while(left<=right && top<=bottom){
+           for(int i=left;i<=right;i++){
+               ans.push_back(matrix[top][i]);
+           }
+           top++;
+           for(int i=top;i<=bottom;i++){
+               ans.push_back(matrix[i][right]);
+           }
+           right--;
+           if(top<=bottom){
+               for(int i=right;i>=left;i--){
+                   
+                   ans.push_back(matrix[bottom][i]);
+               }
+               bottom--;
+           }
+           if(left<=right){
+               for(int i=bottom;i>=top;i--){
+                   ans.push_back(matrix[i][left]);
+               }
+               left++;
+           }
+       }
+       return ans;
+       
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main() {
     int t;
     cin>>t;
@@ -76,4 +69,5 @@ int main() {
         cout<<endl;
     }
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
